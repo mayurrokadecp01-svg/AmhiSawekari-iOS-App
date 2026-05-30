81d6f86910aaf27a7fa7e537fd7214300ee89cbe
Mayur <mayur.rokade@newvision-software.com>
2026-05-30 11:34:00 +0530

    Initial Commit

---
+++ AmhiSevikari/AmhiSevikariApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmhiSevikariApp.swift
+//  AmhiSevikari
+//
+//  Created by Mayur Rokade on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AmhiSevikariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
